b24d80baa5ce4a31dd6417b524ccc4b37ee5f5a4
Aruna <aruna.evan15@gmail.com>
2021-10-18 20:17:10 -0700

    commit tree f06e9db3bc8614522bcabf638c32392dae576c15

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [items, setItems] = useState(itemData);
@@ -11,10 +12,21 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function onItemFormSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name: e.target.name.value,
+      category: e.target.category.value
+    }
+    // would add to database here
+    setItems([newItem, ... items]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={onItemFormSubmit}/>
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,38 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
+  const [search, setSearch] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [itemForm, setItemForm] = useState({
+    category: "Produce",
+    name: ""
+  });
+
+  function onSearchChange(e) {
+    setSearch(e.target.value);
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function onFormChange(e) {
+    setItemForm({...itemForm, [e.target.name]: e.target.value})
+  }
+
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
-
     return item.category === selectedCategory;
+  }).filter((item) => {
+    if (search === "") { return true; }
+    else { return item.name.includes(search); }
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} name={itemForm.name} category={itemForm.category} onFormChange={onFormChange}/>
+      <Filter onCategoryChange={handleCategoryChange} search={search} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
